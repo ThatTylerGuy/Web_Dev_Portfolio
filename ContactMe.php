6d7cfdb528ae32f888ee54957bf6489a1b0288dd
Tyler Curtis <tylercurtis098@gmail.com>
2023-02-22 22:07:51 -0700

    Fixing PHP

--- ContactMe.php
+++ ContactMe.php
@@ -1,26 +1,17 @@
 <?php
-if (! empty($_POST["send"])) {
 
-    if (! filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-        echo "The email address is invalid.";
-    } else {
-        $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
-        $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-        $subject = filter_var($_POST["subject"], FILTER_SANITIZE_STRING);
-        $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-        $toEmail = "tylercurtis098@gmail.com";
+$mailheader = "From:".$name."<".$email.">\r\n";
 
-        // CRLF injection attack prevention
-        $name = strip_crlf($name);
-        $email = strip_crlf($email);
-        $mailHeaders = "From: " . $name . "<" . $email . ">\r\n";
+$recipient = "tylercurtis098@gmail.com";
 
-        if (mail($toEmail, $subject, $message, $mailHeaders)) {
-            ?>
-    <div id="success">Your contact information is received successfully!</div>
-<?php
-        }
-    }
-}
+mail($recipient, $subject, $message, $mailheader)
+or die("Error!");
+
+echo"Message sent";
+
 ?>
